9a43b752c9d95522400e5e21c484fa4a75e75523
navdeep-15 <navdeep.1@appinventiv.com>
2022-09-18 01:02:24 +0530

    wip bugs

--- src/screens/premium/reactNativeScreens/ChartScreen.tsx
+++ src/screens/premium/reactNativeScreens/ChartScreen.tsx
@@ -5,27 +5,41 @@ import { vw, vh } from '@navdeep/utils/dimensions'
 import screenNames from '@navdeep/utils/screenNames'
 import localImages from '@navdeep/utils/localImages'
 import Header from '@navdeep/components/Header'
-import { CartesianGrid, Line, LineChart, Tooltip, XAxis } from 'recharts'
+// import { Line } from 'react-chartjs-2';
+// import { Chart as ChartJS, LineElement, PointElement, LinearScale, Title } from 'chart.js';
+
+// ChartJS.register(LineElement, PointElement, LinearScale, Title);
+
+const data = {
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+    datasets: [
+        {
+            label: "First dataset",
+            data: [33, 53, 85, 41, 44, 65],
+            fill: true,
+            backgroundColor: "rgba(75,192,192,0.2)",
+            borderColor: "rgba(75,192,192,1)",
+            borderWidth: 1,
+        },
+        {
+            label: "Second dataset",
+            data: [33, 25, 35, 51, 54, 76],
+            fill: false,
+            borderColor: "#742774",
+            borderWidth: 1,
+        }
+    ]
+};
 
 export default function ChartScreen(props: any) {
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: '#121212' }}>
             <Header title={'Charts'} props={props} />
-            <View>
-                <LineChart
-                    width={400}
-                    height={400}
-                    //data={data}
-                    margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
-                >
-                    <XAxis dataKey="name" />
-                    <Tooltip />
-                    <CartesianGrid stroke="#f5f5f5" />
-                    <Line type="monotone" dataKey="uv" stroke="#ff7300" yAxisId={0} />
-                    <Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1} />
-                </LineChart>
-            </View>
+            {/* <View style={{ backgroundColor: 'pink', width: '100%', height: '80%' }}> */}
+            {/* <Line data={data} /> */}
+            {/* </View> */}
         </SafeAreaView>
+
     )
 }
 
